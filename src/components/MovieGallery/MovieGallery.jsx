87aeb6af20981442422ca399ef5c962dc5b53330
YacineYldrm <yacine@mailbox.org>
2023-12-06 11:10:00 +0100

    design realised

--- src/components/MovieGallery/MovieGallery.jsx
+++ src/components/MovieGallery/MovieGallery.jsx
@@ -8,12 +8,14 @@ const MovieGallery = ({movieData}) => {
         <section className="gallery_wrapper">
             {movieData.map((movie) => (
             <div className="movie_card_wrapper" key={uuidv4()}>
-                <h2>{movie.title}</h2>
-                <h3>{movie.year}</h3>
-                <h4>{movie.director}</h4>
-                <h4>{movie.duration}</h4>
-                <h4>{movie.rate}</h4>
                 <div>
+                    <h2>{movie.title}</h2>
+                    <h3>{movie.year}</h3>
+                    <h4>{movie.director}</h4>
+                    <h4>{movie.duration}</h4>
+                    <h4>{movie.rate}</h4>
+                </div>
+                <div className="genre_wrapper">
                     {movie.genre.map((genre) => (
                         <p key={uuidv4()}>{genre}</p>
                     
